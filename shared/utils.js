e949121688986f3ca7622c805f66c119a59d465a
mrandyron <cevallos.andy@gmail.com>
2022-04-07 22:27:10 -0400

    Refactor

--- shared/utils.js
+++ shared/utils.js
@@ -1,4 +1,5 @@
 const constants = require('../shared/constants');
+const boom = require('@hapi/boom');
 
 // Para usar estos métodos es importante que se mapeen los
 // tags en los archivos translation.json
@@ -8,15 +9,21 @@ function getErrorByPathOrm(errors, req) {
         const message = constants.ORM_VALIDATION.find((res) =>
             res.path === error.path
             && res.validatorKey === error.validatorKey);
-            console.log(req.t(message.translateKey, 'message.translateKey'))
-        return error.message = req.t('PIN_ID_UNIQUE');
+        return error.message = req.t(message.translateKe);
     });
     return errors;
 }
 
 function translateBoom(err, req) {
-    err.output.payload.message = req.t(err.output.payload.message);
+    err.output.payload.message = req.t(err.output.payload.message.toUpperCase());
     return err;
 }
 
-module.exports = { getErrorByPathOrm, translateBoom }
+function userTokenValidate(userId, idToken) {
+    if (userId != idToken) {
+        throw boom.unauthorized('UNAUTHORIZED');
+    }
+}
+
+
+module.exports = { getErrorByPathOrm, translateBoom, userTokenValidate }
